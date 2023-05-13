9b2088d9433884b75a941a1966e18bcf3eab85fd
Poroshin Dmitrii <DPoroshin@sportmasterlab.net>
2023-05-13 16:18:04 +0600

    feat(ui): card draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './ui/button/button.component';
+import { CardComponent } from './ui/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
