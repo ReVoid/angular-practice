e5ec36b46a514d95142ba33a3a71c82047326ae0
Poroshin Dmitrii <DPoroshin@sportmasterlab.net>
2023-05-15 21:58:32 +0600

    refactor(ui): card as a module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './ui/button/button.component';
-import { CardComponent } from './ui/card/card.component';
+import { CardModule } from "./ui/card/card.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ui/card/card.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from "./card.component";
+import { CardTitleComponent } from './card-title/card-title.component';
+
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    CardTitleComponent,
+  ],
+  exports: [
+    CardComponent,
+    CardTitleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CardModule { }
