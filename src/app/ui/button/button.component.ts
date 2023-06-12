68c1c09b04cbc817d51480c3a4fac142d6a24b0c
Poroshin Dmitrii <DPoroshin@sportmasterlab.net>
2023-06-12 13:39:01 +0300

    refactor(ui): native element container for button and anchor

--- src/app/ui/button/button.component.ts
+++ src/app/ui/button/button.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'button[app-button], a[app-button]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
-
+  @HostBinding('class')
+  get classNames() {
+    return 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800';
+  }
 }
