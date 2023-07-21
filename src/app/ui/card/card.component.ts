820f8b2c64c87cba9619bb7dac21bc680dd27ad0
Poroshin Dmitrii <DPoroshin@sportmasterlab.net>
2023-07-21 19:15:21 +0300

    feat(ui): card content

--- src/app/ui/card/card.component.ts
+++ src/app/ui/card/card.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, Input, ViewChild, TemplateRef } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
+  selector: '[app-card]',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
   @Input()
   title: string = ''
+
+  @ViewChild('footer')
+  footer?: TemplateRef<HTMLElement>;
+
+  @HostBinding('class')
+  get classNames(): string {
+    return 'block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700';
+  }
 }
