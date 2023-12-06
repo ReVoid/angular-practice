312db684d3061f73b1ec506c2d5309b838ba0158
Poroshin Dmitrii <DPoroshin@sportmasterlab.net>
2023-12-06 14:54:37 +0300

    feat(view): post create form draft

--- src/app/views/post-create-page/post-create-page.component.ts
+++ src/app/views/post-create-page/post-create-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators} from "@angular/forms";
+import { IPost, PostService } from "../../services/post.service";
+
+type PostForm = { [K in keyof Omit<IPost, 'id' | 'userId'>]: FormControl<IPost[K]> };
 
 @Component({
   selector: 'app-post-create-page',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-create-page.component.scss']
 })
 export class PostCreatePageComponent {
+  public form: FormGroup<PostForm>;
 
+  constructor(private repository: PostService) {
+    this.form = new FormGroup<PostForm>({
+      title: new FormControl<string>('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(150),
+        ],
+        nonNullable: true,
+      }),
+      body: new FormControl<string>('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(350),
+        ],
+        nonNullable: true,
+      }),
+    })
+  }
+
+  onSubmit() {
+    this.repository.create({
+        userId: 1, // TODO: remove static value
+        ...this.form.getRawValue(),
+      }).subscribe();
+  }
 }
