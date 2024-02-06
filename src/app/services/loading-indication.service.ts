44a1efd6b3c5f1d8dfe0fd91b5e3f0c2082eb5e1
Poroshin Dmitrii <DPoroshin@sportmasterlab.net>
2024-02-06 17:29:57 +0300

    feat(ui): min loading indication delay

--- src/app/services/loading-indication.service.ts
+++ src/app/services/loading-indication.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Observable, of, tap, concatMap, finalize, delay} from "rxjs";
+import {
+  Observable,
+  BehaviorSubject,
+  of,
+  tap,
+  concatMap,
+  finalize,
+  delay,
+} from 'rxjs';
 
 @Injectable()
 export class LoadingIndicationService {
@@ -8,11 +16,13 @@ export class LoadingIndicationService {
 
   loading$ = this._state.asObservable();
 
+  // TODO: Add injectable configs
   show<T>(process$: Observable<T>): Observable<T> {
     return of(null).pipe(
       delay(0), // quick fix of the error https://angular.io/errors/NG0100
       tap(() => this._state.next(true)), // show an indicator before loading starts
       concatMap(() => process$), // add awaitable process
+      delay(300), // minimum time to make an indicator noticeable by user
       tap(() => this._state.next(false)), // hide an indicator in the end
       finalize(() => this._state.next(false)), // do the same either on complete, error or unsubscribe
     );
