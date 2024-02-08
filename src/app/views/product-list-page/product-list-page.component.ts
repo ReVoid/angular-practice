cbf5f95e4354a588046a91314f672191ea7bb943
Poroshin Dmitrii <DPoroshin@sportmasterlab.net>
2024-02-08 13:04:34 +0300

    refactor(view): pagination replaced on product-list-page.component.ts

--- src/app/views/product-list-page/product-list-page.component.ts
+++ src/app/views/product-list-page/product-list-page.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from "@angular/forms";
 import {
   Observable,
-  BehaviorSubject,
   switchMap,
   catchError,
   of,
@@ -18,17 +17,20 @@ import { LoadingIndicationService } from "../../services/loading-indication.serv
 
 import { IProductPagedList, ProductService } from "../../services/product.service";
 
-type Products = IProductPagedList['products'];
-
 @Component({
   selector: 'app-product-list-page',
   templateUrl:  './product-list-page.component.html',
   styleUrls: ['./product-list-page.component.scss']
 })
 export class ProductListPageComponent implements OnInit {
-  public pages$: Observable<Array<number>> = of([]);
+  constructor(
+    private readonly repository: ProductService,
+    private readonly loading: LoadingIndicationService,
+  ) {}
 
-  public currentPage$: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+  public query = new FormControl<string>('', { nonNullable: true });
+
+  public page = new FormControl<number>(1, { nonNullable: true });
 
   public data$: Observable<IProductPagedList> = of({
     products: [],
@@ -37,16 +39,7 @@ export class ProductListPageComponent implements OnInit {
     total: 0,
   });
 
-  public page = new FormControl<number>(1, { nonNullable: true });
-
-  public products$: Observable<Products> = of([]);
-
-  public query = new FormControl<string>('', { nonNullable: true });
-
-  constructor(
-    private readonly repository: ProductService,
-    private readonly loading: LoadingIndicationService,
-  ) {}
+  public payload$!: Observable<[string, number]>;
 
   private fetchData(query: string, page: number = 1, size: number = 10): Observable<IProductPagedList> {
     const result$ = query
@@ -64,16 +57,22 @@ export class ProductListPageComponent implements OnInit {
       distinctUntilChanged(), // prevent duplicated values
     );
 
-    const payload$ = combineLatest([
-      query$,
-      this.currentPage$,
-    ]).pipe(
-      startWith<[string, number]>([this.query.value, 1]), // Triggers request with an initial payload
+    const page$ = this.page.valueChanges.pipe(
+      debounceTime(400), // prevent frequent requests
     );
 
-    this.data$ = payload$.pipe(
+    this.payload$ = combineLatest([
+      query$.pipe(
+        startWith(this.query.value), // triggers first request
+      ),
+      page$.pipe(
+        startWith(this.page.value), // triggers first request
+      ),
+    ]);
+
+    this.data$ = this.payload$.pipe(
       switchMap(([query, page]) => this.fetchData(query, page)),
-      shareReplay(1), // prevent duplicated requests
+      shareReplay(), // prevent duplicated requests
       catchError(() => of({
         products: [],
         limit: 0,
@@ -81,16 +80,5 @@ export class ProductListPageComponent implements OnInit {
         total: 0,
       })), // fallback an empty data on error
     );
-
-    this.products$ = this.data$.pipe(
-      map(i => i.products),
-    );
-
-    this.pages$ = this.data$.pipe(
-      map((data) => {
-        const totalPages = Math.ceil(data.total / data.limit);
-        return [...Array(totalPages).keys()].map(i => i + 1);
-      }),
-    )
   }
 }
